7108bc95baf32513066f1d1ee497f724fb202012
tjweath <t_weatherseed@hotmail.com>
2024-02-02 15:42:25 +0000

    navbar push

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,13 +1,22 @@
 import { Link } from "react-router-dom";
 
+
 export default function NavBar() {
   return (
-    <nav className="nav">
-      <Link to="/listings">Listings</Link>
-      &nbsp; | &nbsp;
-      <Link to="/profile">My Profile</Link>
+    <nav className="navbar">
+      <div className="container d-flex">
+        <Link className="navbar-brand" to="/">
+        <img src="bluelogo.png" alt="iTech Logo" width="140" height="70"/>
+        </Link>
+        <div className="navbar-nav d-flex">
+          <Link className="nav-link" to="/listings">
+            Listings
+          </Link>
+          <Link className="nav-link ml-2" to="/profile">
+            Profile
+          </Link>
+        </div>
+      </div>
     </nav>
   );
 }
-
-
