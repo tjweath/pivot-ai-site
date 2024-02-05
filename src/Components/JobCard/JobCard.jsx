9dcf57f7811ada284cd30b99bc1523f33b76171a
Sylvia Drake-Gill <sylviadrake-gill@sylvias-air.home>
2024-02-05 15:59:44 +0000

    job card styling updates

--- src/Components/JobCard/JobCard.jsx
+++ src/Components/JobCard/JobCard.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import "../../App/App.css"
+import "./JobCard.css"
 
 const JobCard = () => {
   const [jobs, setJobs] = useState([]);
