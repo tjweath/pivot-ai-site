b0f537a92f47cfbb9bf550cfcdb144ed1b212a98
tjweath <t_weatherseed@hotmail.com>
2024-02-05 13:39:38 +0000

    userprofile

--- src/Components/UserProfile/UserProfile.jsx
+++ src/Components/UserProfile/UserProfile.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React from 'react';
+import * as userService from '../../utilities/users-service';
 
-export default function UserProfile() {
+export default function UserProfile({ user, setUser }) {
+
+  function handleLogOut() {
+    userService.logOut();
+    setUser(null);
+  }
+
   return (
-    <div>User Profile</div>
-  )
+    <div>
+      <span>Welcome, {user && user.name}</span>
+      <button onClick={handleLogOut}>Logout</button>
+    </div>
+  );
 }
