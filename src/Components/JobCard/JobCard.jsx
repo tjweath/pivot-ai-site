3d9ce047e82ae9f6bed1d828b24e9c592b2e114a
Sylvia Drake-Gill <sylviadrake-gill@sylvias-air.home>
2024-02-05 15:47:40 +0000

    job card styling

--- src/Components/JobCard/JobCard.jsx
+++ src/Components/JobCard/JobCard.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import "../../App/App.css"
 
 const JobCard = () => {
   const [jobs, setJobs] = useState([]);
+  const [hoveredIndex, setHoveredIndex] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -14,32 +16,55 @@ const JobCard = () => {
           },
         });
 
-        // Assuming the job data is in the 'results' property of the response
         setJobs(response.data.results);
-        console.log(response.data.results)
       } catch (error) {
         console.error('Error fetching job data:', error);
       }
     };
 
     fetchData();
-  }, []); // Empty dependency array ensures that the effect runs only once on mount
+  }, []);
+
+  const handleHover = (index) => {
+    setHoveredIndex(index);
+  }
+
+  const handleLeave = () => {
+    setHoveredIndex(null);
+  }
 
   return (
-    <div>
-    {/* Render your job cards using the 'jobs' state */}
-    {jobs.map((job) => (
-      <div key={job.id} className="border p-3 mb-3">
-        {/* Render individual job card details */}
-        <h2>{job.title}</h2>
-        <p>{job.company.display_name}</p>
-        <p>£{job.salary_max}</p>
-        <p>{job.location.area[3]}</p>
-        {/* Add more job card details as needed */}
-      </div>
-    ))}
-  </div>
-);
+<div>
+      {jobs.map((job, index) => (
+        <div 
+          key={job.id} 
+          className={`job-card ${hoveredIndex === index ? 'hovered' : ''}`}
+          onMouseEnter={() => handleHover(index)}
+          onMouseLeave={handleLeave}
+        >
+          <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+            <div>
+              <img 
+                src="https://i.ibb.co/51SXjcx/UKSpook-Spotters-Logo.jpg" 
+                alt="Company Logo" 
+                className="company-logo" 
+              />
+            </div>
+            <div style={{ marginLeft: '15px', textAlign: 'left' }}>
+              <h2 style={{ color: '#1971c2', margin: 0 }}>{job.title.toUpperCase()}</h2>
+              <h3 style={{ margin: 0 }}>{job.company.display_name.toUpperCase()}</h3>
+              <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+                <p style={{ margin: 0, fontSize: '18px' }}>£{job.salary_max.toLocaleString('en-GB', { maximumFractionDigits: 0 })}</p>
+                <p style={{ paddingLeft: '10px', margin: 0, fontSize: '18px' }}>
+                  {job.location.area[3] || job.location.area[4]}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default JobCard;
