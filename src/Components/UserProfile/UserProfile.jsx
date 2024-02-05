ebecced5be7671e9f7530925657d7026cf0337ba
tjweath <t_weatherseed@hotmail.com>
2024-02-05 15:41:27 +0000

    userprofile

--- src/Components/UserProfile/UserProfile.jsx
+++ src/Components/UserProfile/UserProfile.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import '../../App/App.css';
 import * as userService from '../../utilities/users-service';
+
 
 export default function UserProfile({ user, setUser }) {
 
@@ -9,9 +11,25 @@ export default function UserProfile({ user, setUser }) {
   }
 
   return (
-    <div>
-      <span>Welcome, {user && user.name}</span>
-      <button onClick={handleLogOut}>Logout</button>
+    <div className="card-text-center">
+      <div className="card-header">
+        <img src="cat.png" className="rounded-circle" style={{ width: '150px', height: '150px' }} />
+      </div>
+      <div className="profile-body">
+        <ul className="profile-list profile-name">Name</ul>
+        <ul className="profile-list profile-jobtitle">Job Title:</ul>
+        <ul className="profile-list profile-csalary">Current Salary:</ul>
+        <ul className="profile-list profile-dsalary">Desired Salary:</ul>
+        <ul className="profile-list profile-location">Location:</ul>
+        <ul className="profile-list profile-reloctae">Open to Relocate:</ul>
+        <ul className="profile-list profile-techstack">Tech Stack:</ul>
+      </div>
+      <div>
+        <a href="#" className="btn btn-primary">Edit Profile</a>
+      </div>
+      &nbsp; &nbsp;
+      <h1>My Saved Jobs</h1>
     </div>
   );
 }
+
