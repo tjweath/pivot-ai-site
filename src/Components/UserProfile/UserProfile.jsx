8c21e3c5afc4d60b6ad671530c88c29d952444ed
tjweath <t_weatherseed@hotmail.com>
2024-02-06 14:29:32 +0000

    save function

--- src/Components/UserProfile/UserProfile.jsx
+++ src/Components/UserProfile/UserProfile.jsx
@@ -1,27 +1,38 @@
 import React, { useEffect, useState } from "react";
 import "../../App/App.css";
 import { getUser } from "../../utilities/users-service";
-
 export default function UserProfile({ user, setUser }) {
+  
   const [userData, setUserData] = useState(null);
-
+  const [savedJobs, setSavedJobs] = useState([]);
+  
   useEffect(() => {
     const fetchUserData = async () => {
       const userData = await getUser();
       setUserData(userData);
+      // Fetch saved jobs for the user
+      const savedJobsData = JSON.parse(localStorage.getItem("savedJobs")) || [];
+      setSavedJobs(savedJobsData);
     };
-
     fetchUserData();
   }, []);
+  
+  
   return (
     <div className="card-text-center">
-      <div className="card-header">
+      <div className="card-header"></div>
+      <div>
+        <h1>Hi {userData?.name}!</h1>
       </div>
-        <div>
-          <h1>Hi {userData?.name}! </h1>
-        </div>
-      &nbsp; &nbsp;
       <h1>My Saved Jobs</h1>
+      <ul>
+  {savedJobs.map((job, index) => (
+    <div key={index}>
+      {job.title} - {job.company.display_name}
+    </div>
+  ))}
+</ul>
+
     </div>
   );
 }
