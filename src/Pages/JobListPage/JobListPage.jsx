b21683629b1d2d5d6df97da2ba1f30aa1b8949e1
tamistyping <enquiretam@gmail.com>
2024-02-06 14:45:52 +0000

    update

--- src/Pages/JobListPage/JobListPage.jsx
+++ src/Pages/JobListPage/JobListPage.jsx
@@ -1,7 +1,6 @@
 import axios from "axios";
 import JobCard from "../../Components/JobCard/JobCard";
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 
 export default function JobListPage() {
   const [jobs, setJobs] = useState([]);
