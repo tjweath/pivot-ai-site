b491bbcd472302837ee4998988fc0d5df18930d3
tjweath <t_weatherseed@hotmail.com>
2024-02-06 09:57:33 +0000

    logged in state

--- src/Pages/ProfilePage/ProfilePage.jsx
+++ src/Pages/ProfilePage/ProfilePage.jsx
@@ -2,17 +2,18 @@ import UserProfile from "../../Components/UserProfile/UserProfile"
 import LoginForm from "../../Components/LoginForm/LoginForm"
 import SignUpForm from "../../Components/SignUpForm/SignUpForm"
 import { useState } from "react"
+import { getUser } from "../../utilities/users-service"
 
 
 export default function ProfilePage() {
 // const [showForms, setShowForms] = useState(null)
-const [user, setUser] = useState(null);
+const [user, setUser] = useState(getUser());
 
   return (
     <main>
       {user ? (
         // User is logged in, show UserProfile
-        <UserProfile />
+        <UserProfile setUser={setUser}/>
       ) : (
         // User is not logged in, show SignUpForm and LoginForm
         <div>
