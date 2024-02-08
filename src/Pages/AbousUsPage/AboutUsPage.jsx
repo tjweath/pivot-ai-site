14ee366f428c4a5650d30947df21d5e60e79e97a
tamistyping <enquiretam@gmail.com>
2024-02-08 11:06:33 +0000

    template page update

--- src/Pages/AbousUsPage/AboutUsPage.jsx
+++ src/Pages/AbousUsPage/AboutUsPage.jsx
@@ -6,9 +6,14 @@ export default function AboutUs() {
     <div className="container">
       <h1>About inTech</h1>
       <p>
-        inTech is a job board platform designed specifically for entry-level developers to find job opportunities. Created by Tam, Tim, and Sylvia, Intech offers a user-friendly interface built with JavaScript, CSS, HTML, React, and Bootstrap.
+      Created by Tam, Tim, and Sylvia, inTech is a job board platform designed specifically for entry-level developers to find job opportunities. 
       </p>
+      <div>
+        <h4>Tech Stack used</h4>
+        
+      </div>
       <p>
+        <h4>Features</h4>
         Our platform utilizes the Adzuna API to fetch job listings, providing users with a comprehensive database of available opportunities. Users can save jobs, track their application progress, and update their status (e.g., applied, interviewing) as they navigate their job search journey.
       </p>
     </div>
