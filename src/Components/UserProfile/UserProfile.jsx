10a8e92d6ff6fa353fd92d2f97db61dbd8b300bf
tamistyping <enquiretam@gmail.com>
2024-02-08 12:34:45 +0000

    userprofile checklist

--- src/Components/UserProfile/UserProfile.jsx
+++ src/Components/UserProfile/UserProfile.jsx
@@ -34,7 +34,7 @@ export default function UserProfile({ user, setUser }) {
   };
 
   const getColor = (index) => {
-    const colors = ['#b2f2bb', '#4dabf7', '#1971c2', '#40c057'];
+    const colors = ["#b2f2bb", "#4dabf7", "#1971c2", "#40c057"];
     return colors[index % colors.length];
   };
 
@@ -50,35 +50,77 @@ export default function UserProfile({ user, setUser }) {
           <div key={index} className="job-card-user-profile">
             <Link to={`/job/${job.id}`} state={job} className="Link">
               <div className="job-details">
-                <div style={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
-                  <div className="company-logo" style={{ backgroundColor: getColor(index) }}>
-                  </div>
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    alignItems: "center",
+                  }}
+                >
+                  <div
+                    className="company-logo"
+                    style={{ backgroundColor: getColor(index) }}
+                  ></div>
                   <div style={{ marginLeft: "15px", textAlign: "left" }}>
-                    <h2 className="mobile-text-header" style={{ color: "#1971C2", margin: 0, fontSize: "25px"}}>
+                    <h2
+                      className="mobile-text-header"
+                      style={{ color: "#1971C2", margin: 0, fontSize: "25px" }}
+                    >
                       {job.title.toUpperCase()}
                     </h2>
                     <h3 style={{ margin: 0, color: "black", fontSize: "20px" }}>
                       {job.company.display_name.toUpperCase()}
                     </h3>
-                    <div style={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
-                      <p style={{ margin: 0, fontSize: "20px", color: "black" }}>
+                    <div
+                      style={{
+                        display: "flex",
+                        flexDirection: "row",
+                        alignItems: "center",
+                      }}
+                    >
+                      <p
+                        style={{ margin: 0, fontSize: "20px", color: "black" }}
+                      >
                         £
                         {job.salary_max.toLocaleString("en-GB", {
                           maximumFractionDigits: 0,
                         })}
                       </p>
-                      <p style={{ paddingLeft: "10px", margin: 0, fontSize: "20px", color: "black" }}>
-                        📍 {job.location.area[3] || job.location.area[4] || "UK"}
+                      <p
+                        style={{
+                          paddingLeft: "10px",
+                          margin: 0,
+                          fontSize: "20px",
+                          color: "black",
+                        }}
+                      >
+                        📍{" "}
+                        {job.location.area[3] || job.location.area[4] || "UK"}
                       </p>
                     </div>
                   </div>
                 </div>
               </div>
             </Link>
-            <button type="button" className={`btn ${job.applied ? 'btn-success' : 'btn-primary'} toggle-button`} onClick={() => handleToggleApplication(index)}>
-              {job.applied ? 'Applied' : 'Not Applied'}
+            <button
+              type="button"
+              className="btn btn-danger delete-button"
+              onClick={() => handleDeleteJob(index)}
+            >
+              X
             </button>
-            <button type="button" className="btn btn-danger delete-button" onClick={() => handleDeleteJob(index)}>X</button>
+            <select
+              className={`btn ${
+                job.status === "applied" ? "btn-success" : "btn-primary"
+              } toggle-button custom-select`}
+              value={job.status}
+              onChange={(e) => handleToggleApplication(index, e.target.value)}
+            >
+              <option value="notApplied">Not Applied</option>
+              <option value="applied">Applied</option>
+              <option value="interviewing">Interviewing</option>
+              <option value="hired">Hired</option>
+            </select>
           </div>
         ))}
       </div>
