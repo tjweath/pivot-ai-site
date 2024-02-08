2876f08808528d1a5229c41f82344235f01c50f8
tamistyping <enquiretam@gmail.com>
2024-02-08 15:21:02 +0000

    jotlist update

--- src/Components/JobCard/JobCard.jsx
+++ src/Components/JobCard/JobCard.jsx
@@ -10,42 +10,49 @@ const JobCard = ({ jobs, hoveredIndex, handleHover, handleLeave }) => {
 
   return (
     <div>
-      {jobs.map((job, index) => (
-        <div
-          key={job.id}
-          className={`job-card ${hoveredIndex === index ? "hovered" : ""}`}
-          onMouseEnter={() => handleHover(index)}
-          onMouseLeave={handleLeave}
-        >
-          <Link to={`/job/${job.id}`} state={job} className="Link">
-            <div className="job-card-content">
-              <div style={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
-                <div className="company-logo" style={{ backgroundColor: getColor(index) }}>
-                </div>
-                <div style={{ marginLeft: "15px", textAlign: "left" }}>
-                  <h2 style={{ color: "#1971C2", margin: 0, fontSize: "25px"}}>
-                    {job.title.toUpperCase()}
-                  </h2>
-                  <h3 style={{ margin: 0, color: "black", fontSize: "20px" }}>
-                    {job.company.display_name.toUpperCase()}
-                  </h3>
-                  <div style={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
-                    <p style={{ margin: 0, fontSize: "20px", color: "black" }}>
-                      £
-                      {job.salary_max.toLocaleString("en-GB", {
-                        maximumFractionDigits: 0,
-                      })}
-                    </p>
-                    <p style={{ paddingLeft: "10px", margin: 0, fontSize: "20px", color: "black" }}>
-                      📍 {job.location.area[3] || job.location.area[4] || "UK"}
-                    </p>
+      {jobs.map((job, index) => {
+        const jobTitleWords = job.title.split(" ");
+        const companyWords = job.company.display_name.split(" ");
+        const shortJobTitle = jobTitleWords.slice(0, 3).join(" ");
+        const shortCompanyName = companyWords.slice(0, 2).join(" ");
+
+        return (
+          <div
+            key={job.id}
+            className={`job-card ${hoveredIndex === index ? "hovered" : ""}`}
+            onMouseEnter={() => handleHover(index)}
+            onMouseLeave={handleLeave}
+          >
+            <Link to={`/job/${job.id}`} state={job} className="Link">
+              <div className="job-card-content">
+                <div style={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
+                  <div className="company-logo" style={{ backgroundColor: getColor(index) }}>
+                  </div>
+                  <div style={{ marginLeft: "15px", textAlign: "left" }}>
+                    <h2 className="jobtitle" style={{ color: "#1971C2", margin: 0, fontSize: "23px"}}>
+                      {shortJobTitle.toUpperCase()}
+                    </h2>
+                    <h3 className="companyname" style={{ margin: 0, color: "black", fontSize: "19px" }}>
+                      {shortCompanyName.toUpperCase()}
+                    </h3>
+                    <div style={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
+                      <p style={{ margin: 0, fontSize: "16px", color: "black" }}>
+                        £
+                        {job.salary_max.toLocaleString("en-GB", {
+                          maximumFractionDigits: 0,
+                        })}
+                      </p>
+                      <p style={{ paddingLeft: "10px", margin: 0, fontSize: "16px", color: "black" }}>
+                        📍 {job.location.area[3] || job.location.area[4] || "UK"}
+                      </p>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </Link>
-        </div>
-      ))}
+            </Link>
+          </div>
+        );
+      })}
     </div>
   );
 };
